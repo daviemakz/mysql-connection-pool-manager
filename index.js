847e4dfe0a65b0d94aa25960ff8e7b089a36d68f
David Makuni Ocelot Ubuntu <daviemakz@gmail.com>
2017-08-01 21:44:36 +0100

    migration to node module

---
+++ index.js
@@ -0,0 +1,57 @@
+"use strict";
+
+/*
+
+Below is an example of the options file you have to pass to the module:
+
+options = {
+  idleCheckInterval: ,
+  maxConnextionTimeout: ,
+  idleTimeout: ,
+  errorLimit: ,
+  preInitDelay: ,
+  sessionTimeout: ,
+  mysqlSettings:
+}
+
+*/
+
+// Load library modules
+const Core = require('./lib/pool');
+
+// Build instance
+class Instance extends Core {
+
+  // Initial constructor
+  constructor(options) {
+    // Allow access to 'this'
+    super();
+    // Bind methods
+    this.config = this.config.bind(this);
+    // Build configuration
+    this.config(options);
+  };
+
+  // Function: Assign settings
+  config(options) {
+    // Define required properties
+    const props = ['idleTimeout', 'errorLimit', 'preInitDelay', 'sessionTimeout'];
+    // Validation configuration
+    Object(options).keys.forEach(prop => {
+      if (!props.includes(prop)) { throw new Error(`The required property ${prop} is not defined!`); }
+      if (typeof(prop) !== 'number') { throw new Error(`The property ${prop} is not a number!`); }
+    });
+    // Check mysql settings are defined (we wont check if they are correct, mysql will do that!)
+    if (typeof(options.mysqlSettings) !== 'object') {
+      throw new Error(`MySQL settings are not defined or not an object!`);
+    }
+    // Spread options to this
+    this.options = Object.assign({}, options);
+    // Return
+    return;
+  };
+
+};
+
+// Exports
+module.exports = (options) => new Instance(options);
