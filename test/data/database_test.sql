541cef6ccda95fb1d6d01c3f5ed97a44e8d1423b
David Makuni Ocelot Ubuntu <daviemakz@gmail.com>
2017-08-21 21:49:55 +0100

    travis-ci config

---
+++ test/data/database_test.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS `test_table`;
+CREATE TABLE `test_table` ( `column_one` int(11) NOT NULL AUTO_INCREMENT, `column_two` int(11) NOT NULL DEFAULT '0', PRIMARY KEY (`column_one`) ) ENGINE=MyISAM AUTO_INCREMENT=6437 DEFAULT CHARSET=utf8;
--- test/setup.sh
+++ test/setup.sh
@@ -1,27 +1,24 @@
 # Ensure latest packages
-sudo apt-get -y update; sudo apt-get -y upgrade; sudo apt-get -y dist-upgrade; sudo apt-get -y autoremove; sudo apt-get -y autoclean;
+#sudo apt-get -y update; sudo apt-get -y upgrade; sudo apt-get -y dist-upgrade; sudo apt-get -y autoremove; sudo apt-get -y autoclean;
 
 # Pre setup variables
-sudo echo "debconf debconf/frontend select Noninteractive" | debconf-set-selections
+#sudo echo "debconf debconf/frontend select Noninteractive" | debconf-set-selections
 
 # Default mySQL Settings
-mysqlUser='root'
-mysqlPass='te5amatRasTe'
+mysqlUser='travis'
+mysqlPass=''
 
 # Setup MySQL Configuration
-sudo echo "mysql-server mysql-server/root_password password $mysqlPass" | debconf-set-selections
-sudo echo "mysql-server mysql-server/root_password_again password $mysqlPass" | debconf-set-selections
+# sudo echo "mysql-server mysql-server/root_password password $mysqlPass" | debconf-set-selections
+# sudo echo "mysql-server mysql-server/root_password_again password $mysqlPass" | debconf-set-selections
 
 # Pre installation packages
-sudo apt-get update
-sudo apt-get install --no-install-recommends -y tzdata
-sudo apt-get install -y mysql-server-5.5 apt-utils git
+#sudo apt-get update
+#sudo apt-get install --no-install-recommends -y tzdata
+#sudo apt-get install -y mysql-server-5.5 apt-utils git
 
 # Start mySQL Server
-sudo service mysql start
-
-# Set Correct Timezone
-mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root "-p$mysqlPass" mysql
+#sudo service mysql start
 
 # Setup MySQL Test Database []
 mysql --user="$mysqlUser" --password="$mysqlPass" --database=mysql --execute="DROP DATABASE IF EXISTS database_test; CREATE DATABASE IF NOT EXISTS database_test;"
